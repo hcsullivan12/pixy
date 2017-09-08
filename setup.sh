2575cbc6e0886e9fac022fef26e08214e3856a15
Damian Goeldi <dg.code+git@protonmail.ch>
2017-09-08 16:34:55 +0200

    adapted CMakeLists.txt and setup.sh so all paths to external libraries can be provided via the environment

--- setup.sh
+++ setup.sh
@@ -2,7 +2,10 @@ softwarePath="/home/damian/software"
 rootPath="${softwarePath}/pixy/root"
 genfitPath="${softwarePath}/genfit/GenFit"
 packagesPath="${softwarePath}/packages"
+eigen3IncludePath="${packagesPath}/include/eigen3"
 
 . ${rootPath}/build/bin/thisroot.sh
 . ${genfitPath}/env.sh
-export ROOT_INCLUDE_PATH="${packagesPath}/include/eigen3"
+export ROOT_INCLUDE_PATH="${eigen3IncludePath}"
+export Eigen3_CMAKEDIR="${packagesPath}/share/eigen3/cmake"
+export RapidJSON_CMAKEDIR="${packagesPath}/lib/cmake/RapidJSON"
